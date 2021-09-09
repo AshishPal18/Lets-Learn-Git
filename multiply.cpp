7c47b46c4b154dd7d4a1a960b4a96aa351bc5628
Ashish Pal <palashish1801@gmail.com>
2021-09-09 21:57:59 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m ,n;
+    cout<<"Enter a first number :";
+    cin>>m;
+    cout<<"Enter a second number :";
+    cin>>n;
+    cout<<"Multiply is :"<<m*n;
+    return 0;
+}
